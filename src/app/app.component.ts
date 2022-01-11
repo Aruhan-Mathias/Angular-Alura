45e3c93b536a372687d2e473649d96ffa4310336
Aruhan Mathias <aruhanmathias@hotmail.com>
2022-01-11 11:51:28 -0300

    primeiro capítulo completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'alurapic';
+  url = "../assets/img/leao.jpg"
+  description = "leão com fundo preto"
+  
 }
